127dd2072552831aea76541a0105c12d2a57025d
Mikko <mkkha7@gmail.com>
2016-01-27 21:46:12 +0200

    Private messages

--- chat_scripts.js
+++ chat_scripts.js
@@ -6,9 +6,24 @@ window.onload = function(event){     //document.onload ei toimi jostain syystä
     
     //listen new message" from server
     socket.on('new message',function(data){    //on-funktiolla kuunnellaa, emit-funktiolla lähetetään
-        //Get textarea with id="message"
-        var textArea = document.getElementById('message');
-        textArea.value += data.name + ":" + data.message + "\n";
+        
+        if(data.to == ""){
+            //Get textarea from html with id="message"
+            var textArea = document.getElementById('message');
+            textArea.value += data.name + ":" + data.message + "\n";
+        }
+        else{
+            
+            var sender = document.getElementById('senderName');
+            if(data.to == sender.value || data.name == sender.value){
+                
+                var textArea = document.getElementById('message');
+                textArea.value += data.name + ":" + data.message + "\n";
+            }
+            else{
+                //Textarea is not updated
+            }
+        }
     });
     
     
@@ -19,12 +34,22 @@ window.onload = function(event){     //document.onload ei toimi jostain syystä
     btnSend.onclick = function(){
      
         var msg = document.getElementById('chat_message');
+        var sender = document.getElementById('senderName');
+        var receiver = document.getElementById('receiverName');
+        
         console.log(msg.value);
+        console.log(sender.value);
+        console.log(receiver);
+        
         var dataToServer = {
-            name:'undefined',
-            message:msg.value
+            //name:'undefined',
+            name:sender.value,
+            message:msg.value,
+            to:receiver.value
         }
         //Send message to server
         socket.emit('chat msg',dataToServer);     //'chat msg' voi olla mikä tahansa
+        
+        document.getElementById('chat_message').value = "";
     }
 }
